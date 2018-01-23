9442be285508ad8fcd2c880b756de40cd5bf771c
sskim <suyakim@forsnap.com>
2018-01-23 12:54:14 +0900

    git branch commit & push test

---
+++ src/components/input/InputText.jsx
@@ -0,0 +1,19 @@
+import "./InputText.scss";
+import React, { Component } from "react";
+
+export default class InputText extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    
+    render() {
+        return (
+            <div>
+                커밋 테스트
+            </div>
+        );
+    }
+}
+
+
